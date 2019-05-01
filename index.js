77b42cda3645ad81d62f5b898e31f9db86c26cb9
americanman <snowboardracer7@gmail.com>
2019-04-30 22:54:10 -0400

    Add Second method

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const convertHTML = (str) => {
-//Chaining of replace method with different arguments
-  str = str.replace(/&/g,'&amp;')
-           .replace(/</g,'&lt;')
-           .replace(/>/g,'&gt;')
-           .replace(/"/g,'&quot;')
-           .replace(/'/g,"&apos;");
-return str;
+//  First method using regEx
+//
+//  str = str.replace(/&/g,'&amp;')
+//           .replace(/</g,'&lt;')
+//           .replace(/>/g,'&gt;')
+//           .replace(/"/g,'&quot;')
+//           .replace(/'/g,"&apos;");
+
+// Add second method using object and map
+  htmlEntities={
+    '&':'&amp;',
+    '<':'&lt;',
+    '>':'&gt;',
+    '"':'&quot;',
+    '\'':"&apos;"
+  };
+  //Use map function to return a filtered str with all entities changed automatically.
+  return str.split('').map(item => htmlEntities[item] || item).join('');
 }
 
 // test here
